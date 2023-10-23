371d3ce261e10d12d751fa72c5380abfa2652419
Michal Wronski <michal.robert.wronski@gmail.com>
2023-10-23 18:37:07 +0200

    Initial Commit

---
+++ scrumdinger/scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerApp.swift
+//  scrumdinger
+//
+//  Created by Michal Wronski on 23/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
